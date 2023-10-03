10493bf0c6e7f92d02ed894e357e50cf8103c674
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-03 23:47:51 +0200

    parser: setup queue connection

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -1,38 +1,48 @@
 import express, { Express, Request, Response , Application } from 'express';
-import axios from "axios";
+import * as amqp from 'amqplib';
 
 const app: Application = express();
 const port: string | 8000 = process.env.PORT || 8000;
-const interval: number = 1000/parseInt(process.env.MCL || "1", 10);
-
-const db_url: string = process.env.DB_URL || "http://localhost:8001";
-const my_queue_url: string = process.env.MY_QUEUE_URL || "http://localhost:8002";
-const next_queue_url: string = process.env.NEXT_QUEUE_URL || "http://localhost:8003";
+const rabbitmqUrl = process.env.RABBITMQ_URL || 'amqp://pipeline_broker:p1p3l1n3@localhost/pipeline-vhost';
 
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-app.use(express.json());
-app.post('/parse', async (_req, res) => {
+app.post('/parse', async (req: Request, res: Response) => {
     try {
-        console.log("parsing...");
-        await sleep(interval);
-        let n_attach = Math.floor(Math.random() * 5);
-        let postDbResponse = await  axios.post(db_url + "/insertInfo", {n_attach: n_attach});
+        const connection = await amqp.connect(rabbitmqUrl);
+        const channel = await connection.createChannel();
 
-        const id = postDbResponse.data.id;
-        for (let i: number = 0; i < n_attach; i++) {
-            let enqueueResponse = await axios.post(next_queue_url + '/enqueue', {id: id});
-            console.log(`Enqueue response ${i}: ${enqueueResponse.data.message}`);
-        }
+        const queueName = 'demo-queue';
 
-        await axios.post(my_queue_url + "/dequeue");//.then(response => console.log(response.data));
-        res.json({ message: 'Parse request received successfully'});
-    } catch (error: any) {
-        res.status(400).json({ error: error.message });
+        // Assert the queue in the specified virtual host
+        await channel.assertQueue(queueName, { durable: true, arguments: { 'x-queue-mode': 'lazy' }});
+
+        // Publish a message to the queue
+        const message = 'Hello, RabbitMQ!';
+        channel.sendToQueue(queueName, Buffer.from(message));
+        console.log(`Message sent: ${message}`);
+
+        // Consume messages from the queue
+        channel.consume(queueName, (msg) => {
+            if (msg !== null) {
+                console.log(`Received message: ${msg.content.toString()}`);
+                // You can process the message here
+                channel.ack(msg); // Acknowledge the message
+
+                return res.status(200).send(msg.content.toString());
+            }
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
     }
 });
+
+
+
+app.use(express.json());
 app.listen(port, () => {
     console.log(`Message parser launched at http://localhost:${port}`);
 });
