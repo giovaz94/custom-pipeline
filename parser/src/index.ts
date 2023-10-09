a10998666fb40e2307b409ed9ca13881563c410a
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-09 15:22:22 +0200

    feat(parse): n_requests

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -6,19 +6,23 @@ const dbUrl = process.env.DB_URL || 'http://localhost:3200';
 console.log(dbUrl);
 const interval = 1000/parseInt(process.env.MCL as string, 10);
 
+let n_request = 0;
+
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 startConsumer(queueName, async (task) => {
-    console.log(` ~[*] Received task: ${JSON.stringify(task)}`);
+    // console.log(` ~[*] Received task: ${JSON.stringify(task)}`);
+    n_request++;
+    console.log(n_request);
     await sleep(interval);
     const n_attach = Math.floor(Math.random() * 5);
     const insertInfoUrl = dbUrl + "/insertInfo";
     await axios.post(insertInfoUrl, {n_attach: n_attach}).then(response => {
         const id = response.data.id;
-        console.log(`   ~[*] Inserted ${n_attach} attachments with id ${id}`);
+        // console.log(`   ~[*] Inserted ${n_attach} attachments with id ${id}`);
         /*for (let i = 0; i < n_attach; i++) { // TODO:Enqueue to next service }*/
     });
-    console.log(` ~[*] Task processed successfully!`);
+    // console.log(` ~[*] Task processed successfully!`);
 });
