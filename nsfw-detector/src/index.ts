3392901f9bbae9bfaf257b65bb53c6b544b9e68f
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-15 23:39:48 +0200

    feat(image-recognizer): implement logic

--- nsfw-detector/src/index.ts
+++ nsfw-detector/src/index.ts
@@ -1,6 +1,6 @@
 import {addInQueue, startConsumer} from "./queue/queue";
 
-const queueName = process.env.QUEUE_NAME || 'imagerec.queue';
+const queueName = process.env.QUEUE_NAME || 'nsfwdet.queue';
 const interval = 1000/parseInt(process.env.MCL as string, 10);
 
 const queueTypeImageAnalyzer = process.env.QUEUE_IMAGE_ANALYZER || 'imageanalyzer.req';
@@ -13,7 +13,7 @@ startConsumer(queueName, async (task) => {
     console.log(` ~ [*] Received a new request wit id ${task.data}`);
     await sleep(interval);
     addInQueue('pipeline.direct', queueTypeImageAnalyzer, {
-        data: {response: "Image recognized", id: task.data, type: "imageRecognizer"},
+        data: {response: "NSFW checked", id: task.data, type: "nsfwDetector"},
         time: new Date().toISOString(),
     });
     console.log(` ~ [!] Done processing image with id ${task.data}`);
