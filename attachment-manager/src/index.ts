6d71108265cac592196b0614b1bd6291d760c9d9
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-15 21:30:21 +0200

    feat(attachment-manager): send task to the next queue

--- attachment-manager/src/index.ts
+++ attachment-manager/src/index.ts
@@ -1,8 +1,9 @@
-import {startConsumer} from "./queue/queue";
+import {addInQueue, startConsumer} from "./queue/queue";
 
-const queueName = process.env.QUEUE_NAME || 'virusscan.queue';
+const queueName = process.env.QUEUE_NAME || 'attachmentman.queue';
 const interval = 1000/parseInt(process.env.MCL as string, 10);
 const exchangeName = process.env.EXCHANGE_NAME || 'pipeline.direct';
+const queueType = process.env.QUEUE_TYPE || 'imageanalyzer.req';
 
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -10,6 +11,6 @@ function sleep(ms: number) {
 
 startConsumer(queueName, async (task) => {
     await sleep(interval);
-    console.log("  ~[*] Request handled successfully");
+    addInQueue(exchangeName, queueType, task)
 });
 
