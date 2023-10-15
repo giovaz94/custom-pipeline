e0251d4ceaffa6815ce1cced9ab7f299c247cdcd
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-15 11:45:37 +0200

    feat(virus-scanner): add rabbitmq connection's configurations

--- virus-scanner/src/index.ts
+++ virus-scanner/src/index.ts
@@ -1,2 +1,9 @@
-console.log("Hello world!");
+import {startConsumer} from "./queue/queue";
+
+const queueName = process.env.QUEUE_NAME || 'virusscan.queue';
+const dbUrl = process.env.DB_URL || 'http://localhost:3200';
+
+startConsumer(queueName, async (task) => {
+   console.log(` ~[*] Received task: ${JSON.stringify(task)}`);
+});
 
---
+++ virus-scanner/src/queue/queue.ts
@@ -0,0 +1,19 @@
+import RabbitMQConnection from "../configuration/rabbitmq.config";
+import {Connection, Channel, ConsumeMessage} from "amqplib";
+
+// Define the structure of the task to submit to the entrypoint
+export type TaskType = {
+    data: any;
+    time: String;
+}
+
+export async function startConsumer(queueName: string, processTask: (task: TaskType) => void) {
+    const channel: Channel = await RabbitMQConnection.getChannel();
+    channel.consume(queueName, (msg: ConsumeMessage | null) => {
+        if (msg !== null) {
+            const taskData: TaskType = JSON.parse(msg.content.toString());
+            processTask(taskData);
+            channel.ack(msg);
+        }
+    });
+}
