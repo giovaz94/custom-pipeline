7485e38297a9c9fadf6618b60ec8e263a2af103c
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-22 22:35:18 +0100

    feat: add request funciton

--- local_scaling/main.py
+++ local_scaling/main.py
@@ -12,28 +12,29 @@ def guard() -> None:
     """
     print("Monitoring the system...")
     while True:
-        # inbound_workload = self.get_inbound_workload()
+        inbound_workload = get_service_info()
         # current_mcl = self.scaler.get_mcl()
         # print(f"Current mcl: {current_mcl}")
         # print(f"Inbound workload: {inbound_workload}")
         # if self.should_scale(inbound_workload, current_mcl):
         # print("Scaling the system...")
         # self.scaler.process_request(inbound_workload)
+        print(f"Inbound workload: {inbound_workload}")
         print("Checked sys condition")
         time.sleep(SLEEP_TIME)
 
 
-def get_inbound_workload() -> int:
+def get_service_info() -> int:
     """
     Return the inbound workload of the system,
     querying the external monitoring system.
     """
-    monitor_url = os.environ.get("MONITOR_URL")
+    # monitor_url = os.environ.get("MONITOR_URL")
+    monitor_url = "http://localhost:8011"
     if monitor_url:
-        endpoint = monitor_url + "/inboundWorkload"
+        endpoint = monitor_url + "/inbound-workload"
         response = requests.get(endpoint).json()
-        print(f"Inbound workload registered {response['inbound_workload']}")
-        return response["inbound_workload"]
+        return response
     else:
         raise Exception("MONITOR_URL not set")
 
