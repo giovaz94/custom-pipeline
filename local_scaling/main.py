4d9b042b2ef0ec37e32cb4b3f8da16c2593975fe
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-23 15:04:25 +0100

    feat: finish development of local_scaling component

--- local_scaling/main.py
+++ local_scaling/main.py
@@ -1,43 +1,91 @@
 import time
+
+import yaml
+from kubernetes import client, config
+from deployment import deploy_pod, delete_pod_by_image
 import os
 import requests
 
-SLEEP_TIME = 2
-
-
-def guard() -> None:
-    """
-    This method is executed in a separate thread.
-    Check the conditions of the system and eventually scale it.
-    """
-    print("Monitoring the system...")
-    while True:
-        inbound_workload = get_service_info()
-        # current_mcl = self.scaler.get_mcl()
-        # print(f"Current mcl: {current_mcl}")
-        # print(f"Inbound workload: {inbound_workload}")
-        # if self.should_scale(inbound_workload, current_mcl):
-        # print("Scaling the system...")
-        # self.scaler.process_request(inbound_workload)
-        print(f"Inbound workload: {inbound_workload}")
-        print("Checked sys condition")
-        time.sleep(SLEEP_TIME)
-
-
-def get_service_info() -> int:
-    """
-    Return the inbound workload of the system,
-    querying the external monitoring system.
-    """
-    # monitor_url = os.environ.get("MONITOR_URL")
-    monitor_url = "http://localhost:8011"
-    if monitor_url:
-        endpoint = monitor_url + "/inbound-workload"
-        response = requests.get(endpoint).json()
-        return response
-    else:
-        raise Exception("MONITOR_URL not set")
-
-
 if __name__ == '__main__':
-    guard()
+
+    SLEEP_TIME = float(os.environ.get("SLEEP_TIME"))
+    COMPONENT_MCL = float(os.environ.get("COMPONENT_MCL"))
+    COMPONENT_MF = float(os.environ.get("COMPONENT_MF"))
+    K_BIG = int(os.environ.get("K_BIG"))
+    K = int(os.environ.get("K"))
+    MONITOR_URL = os.environ.get("MONITOR_URL")
+
+    config.load_incluster_config()
+    k8s_client = client.CoreV1Api()
+
+
+    def guard(starting_mcl, starting_instances) -> None:
+        """
+        This method is executed in a separate thread.
+        Check the conditions of the system and eventually scale it.
+        """
+        print("Monitoring the system...")
+        current_mcl = starting_mcl
+        number_of_instances = starting_instances
+        while True:
+            inbound_workload = get_inbound_workload()
+            if should_scale(inbound_workload, current_mcl):
+                print("Scaling the system...")
+                print(f"Current MCL: {current_mcl}")
+                print(f"Target workload: {inbound_workload}")
+                print(f"N. of instances: {number_of_instances}")
+                instances, mcl = configure_system(inbound_workload)
+
+                if instances > number_of_instances:
+                    for _ in range(instances - number_of_instances):
+                        deploy_pod(k8s_client, "manifest.yaml")
+                elif instances < number_of_instances:
+                    with open("manifest.yaml", 'r') as manifest_file:
+                        for _ in range(number_of_instances - instances):
+                            pod_manifest = yaml.safe_load(manifest_file)
+                            print(pod_manifest)
+                            image_name = pod_manifest["spec"]["containers"][0]["image"]
+                            delete_pod_by_image(k8s_client, image_name)
+
+                number_of_instances = instances
+                current_mcl = mcl
+
+            time.sleep(SLEEP_TIME)
+
+
+    def get_inbound_workload() -> int:
+        """
+        Return the inbound workload of the system,
+        querying the external monitoring system.
+        """
+        if MONITOR_URL:
+            endpoint = MONITOR_URL + "/inbound-workload"
+            response = requests.get(endpoint).json()
+            return response["inboundWorkload"]
+        else:
+            raise Exception("MONITOR_URL not set")
+
+
+    def should_scale(inbound_workload, curr_mcl) -> bool:
+        return inbound_workload - (curr_mcl - K_BIG) > K or \
+            (curr_mcl - K_BIG) - get_inbound_workload() > K
+
+
+    def configure_system(target_workload) -> tuple[int, int]:
+        instances = 1
+        mcl = estimate_mcl(instances)
+        while not configuration_found(mcl, target_workload, K_BIG):
+            instances += 1
+            mcl = estimate_mcl(instances)
+        return instances, mcl
+
+
+    def estimate_mcl(deployed_instances) -> int:
+        return deployed_instances * COMPONENT_MCL / COMPONENT_MF
+
+
+    def configuration_found(sys_mcl, target_workload, k_big) -> bool:
+        return sys_mcl - (target_workload + k_big) >= 0
+
+
+    guard(COMPONENT_MCL, 1)
