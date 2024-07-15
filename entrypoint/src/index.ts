c424350f67014d27693c06617ee67e675225e4d5
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-16 01:20:16 +0200

    attempt

--- entrypoint/src/index.ts
+++ entrypoint/src/index.ts
@@ -78,15 +78,19 @@ app.post('/start', (req: Request, res: Response) => {
         while(index < workload.length && !stop) {
             const r = workload[index++];
             console.log(`Sending ${r} requests per second`);
+            let start =  new Date();
             for (let i = 0; i < r; i++) {
                 const task: TaskType = {
                     data: req.body.id,
                     time: new Date().toISOString()
                 }
+                await new Promise(resolve => setTimeout(resolve, 1000/r));
                 addInQueue(exchangeName, queueType, task);
                 parser_requests.inc();
             }
-            await new Promise(resolve => setTimeout(resolve, 1000));
+            let stop = new Date();
+            const leftover = stop.getTime() - start.getTime();
+            await new Promise(resolve => setTimeout(resolve, leftover));
         }
     })();
     return res.status(201).send("Start simulation...");
