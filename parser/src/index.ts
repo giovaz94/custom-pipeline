3fddf77e2ce950912142467ab4e565782f29cc94
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-18 22:48:14 +0200

    fix: delete supplementary  ack

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -60,7 +60,7 @@ startConsumer(queueName, async (channel: Channel) => {
         await sleep(interval);
         let id = v4();
         const n_attach = Math.floor(Math.random() * 5);
-        channel.ack(msg);
+
         const start: Date =  new Date();
         const taskData: TaskType = JSON.parse(msg.content.toString());
         if(n_attach == 0) {
