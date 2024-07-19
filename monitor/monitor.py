7eb78a38b218a145ab8f29c65f106f224b62eb5a
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-19 19:31:57 +0200

    fix metrics

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -33,24 +33,18 @@ class Logger:
         """
         print("Logging started")
         init_val = self._execute_prometheus_query("sum(http_requests_total_parser)")
-        init_val_completed = self._execute_prometheus_query("sum(last_over_time(message_analyzer_complete_message[10s]))")
-        init_val_latency = self._execute_prometheus_query("sum(last_over_time(http_response_time_sum[10s]))")
         sl = self.sleep
         started = False
         time_difference_ms = 0
         while True:
             start = time.time()
             tot = self._execute_prometheus_query("sum(http_requests_total_parser)")
-            completed = self._execute_prometheus_query("sum(last_over_time(message_analyzer_complete_message[10s]))")
-            latency = self._execute_prometheus_query("sum(last_over_time(http_response_time_sum[10s]))")
+            completed = self._execute_prometheus_query("sum(rate(message_analyzer_complete_message[10s]))")
+            latency = self._execute_prometheus_query("sum(rate(http_response_time_sum[10s]))")
             window_inbound = (tot-init_val)/10
-            window_completed = completed - init_val_completed
-            window_latency = latency - init_val_latency
-            print("INBOUND: " + str(window_inbound) + " COMPLETED: " + str(window_completed) + " AVG LAT: " + str(window_latency/(window_completed if window_completed > 0 else 1)))
+            print("INBOUND: " + str(window_inbound) + " COMPLETED: " + str(completed) + " AVG LAT: " + str(latency/(completed if completed > 0 else 1)))
             if tot - init_val > 0 or started:
                 init_val = tot if started else init_val
-                init_val_completed = completed if started else init_val_completed
-                init_val_latency = latency if started else init_val_latency
                 sl = 10 if started else 9
                 started = True
                 stop = time.time()
