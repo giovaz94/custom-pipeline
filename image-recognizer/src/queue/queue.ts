2597bb11ba0bf40256f8ed1c0406065195c9ca44
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-19 16:35:18 +0200

    refactor: change closeConnection to image-recognizer

--- image-recognizer/src/queue/queue.ts
+++ image-recognizer/src/queue/queue.ts
@@ -1,5 +1,5 @@
 import RabbitMQConnection from "../configuration/rabbitmq.config";
-import {ConsumeMessage, Channel} from "amqplib";
+import {ConsumeMessage, Channel, Replies} from "amqplib";
 
 // Define the structure of the task to submit to the entrypoint
 export type TaskType = {
@@ -9,6 +9,8 @@ export type TaskType = {
 
 export let queue: ConsumeMessage[] = [];
 export let pendingPromises: ((item: ConsumeMessage) => void)[] = [];
+
+var consume: Replies.Consume;
 
 async function enqueue(item: ConsumeMessage): Promise<void> {
     if (pendingPromises.length > 0) {
@@ -28,9 +30,9 @@ export async function dequeue(): Promise<ConsumeMessage> {
 }
 
 export function startConsumer(queueName: string, processTask: (channel: Channel) => void) {
-    RabbitMQConnection.getChannel().then((channel: Channel) => {
+    RabbitMQConnection.getChannel().then(async (channel: Channel) => {
         // channel.prefetch(50);
-        channel.consume(queueName, async (msg: ConsumeMessage | null) => {
+        consume = await channel.consume(queueName, async (msg: ConsumeMessage | null) => {
             if (msg !== null) {
                 channel.ack(msg);
                 enqueue(msg);
@@ -50,7 +52,8 @@ export function addInQueue(
     })
 }
 
-
 export async function closeConnection() {
-    RabbitMQConnection.getChannel().then((channel: Channel) => channel.close());
+    RabbitMQConnection.getChannel().then(
+        (channel: Channel) => channel.cancel(consume.consumerTag)
+    );
 }
