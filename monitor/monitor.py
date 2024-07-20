767efb98d2d704888c6995d7c88f8d1b45bf3963
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-21 01:41:34 +0200

    prefetch 50

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -39,8 +39,8 @@ class Logger:
         while True:
             start = time.time()
             tot = self._execute_prometheus_query("sum(http_requests_total_parser)")
-            completed = self._execute_prometheus_query("sum(rate(message_analyzer_complete_message[10s]))")
-            latency = self._execute_prometheus_query("sum(rate(http_response_time_sum[10s]))")
+            completed = self._execute_prometheus_query("sum(increase(message_analyzer_complete_message[10s]))")
+            latency = self._execute_prometheus_query("sum(increase(http_response_time_sum[10s]))")
             window_inbound = (tot-init_val)/10
             print("INBOUND: " + str(window_inbound) + " COMPLETED: " + str(completed) + " AVG LAT: " + str(latency/(completed if completed > 0 else 1)))
             if tot - init_val > 0 or started:
