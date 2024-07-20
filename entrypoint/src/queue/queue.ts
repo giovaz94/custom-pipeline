f8129d28c7a9bcd1f718fa5e9bc9e8bfc48e0162
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-20 22:09:20 +0200

    remove expiration

--- entrypoint/src/queue/queue.ts
+++ entrypoint/src/queue/queue.ts
@@ -13,7 +13,7 @@ export function addInQueue(
     task: TaskType
 ) {
     RabbitMQConnection.getChannel().then((channel: Channel) => {
-        const isPublished = channel.publish(exchangeName, type ,Buffer.from(JSON.stringify(task)), {expiration: 3000});
+        const isPublished = channel.publish(exchangeName, type ,Buffer.from(JSON.stringify(task)));
         if (!isPublished) {
             console.log('Message rejected');
         }
