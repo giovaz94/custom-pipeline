4a229e246e1b5d23c136688443d822ec15faaa17
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-20 13:31:26 +0200

    fid

--- rabbitmq/init.sh
+++ rabbitmq/init.sh
@@ -5,10 +5,16 @@ set -e
 rabbitmq-server -detached
 
 # Wait for RabbitMQ to start up
-sleep 10
+echo "Waiting for RabbitMQ to be ready..."
+until rabbitmqctl status >/dev/null 2>&1; do
+    sleep 1
+done
 
 # Set the queue length policy
 rabbitmqctl set_policy --vhost pipeline-vhost queue-length "^.*\.queue$" '{"max-length":50}' --priority 0 --apply-to queues
 
+echo "RabbitMQ policy set successfully."
+
 # Bring RabbitMQ server to the foreground
 rabbitmq-server
+
