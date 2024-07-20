de78870b9c03756871b72fcca4e335a58751ccbb
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-20 12:44:57 +0200

    fix

---
+++ rabbitmq/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Start RabbitMQ server in the background
+rabbitmq-server -detached
+
+# Wait for RabbitMQ to start up
+sleep 10
+
+# Set the queue length policy
+rabbitmqctl set_policy -p pipeline-vhost queue-length "^.*\.queue$" '{"max-length":50}' --priority 0 --apply-to queues
+
+# Bring RabbitMQ server to the foreground
+rabbitmq-server
