6c9167db087c1184da5a2c4f112cd666a759bde8
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-23 17:34:49 +0200

    feature: add close connection to nsfw-detector

--- nsfw-detector/src/index.ts
+++ nsfw-detector/src/index.ts
@@ -1,4 +1,13 @@
-import {addInQueue, closeConnection, dequeue, startConsumer, TaskType, queue, pendingPromises} from "./queue/queue";
+import {
+    addInQueue,
+    cancelConnection,
+    dequeue,
+    startConsumer,
+    TaskType,
+    queue,
+    pendingPromises,
+    closeConnection
+} from "./queue/queue";
 import express, { Application } from 'express';
 import * as prometheus from 'prom-client';
 import {ConsumeMessage} from "amqplib";
@@ -45,8 +54,9 @@ startConsumer(queueName, async (channel) => {
 
 process.on('SIGINT', async () => {
     console.log(' [*] Exiting...');
-    closeConnection();
+    cancelConnection();
     while(pendingPromises.length > 0 || queue.length > 0) await sleep(5000);
     await sleep(5000);
+    await closeConnection();
     process.exit(0);
 });
