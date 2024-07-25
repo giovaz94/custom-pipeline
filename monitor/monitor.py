1290952b9bc6bf36eb39ecced61b276441cb4d0c
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-25 22:25:00 +0200

    refactor: add expiration

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -54,7 +54,7 @@ class Logger:
 
 if __name__ == "__main__":
 
-    prometheus_service_address = "143.198.251.80"
+    prometheus_service_address = "152.42.151.89"
     prometheus_service_port = 8080
     prometheus_url = f"http://{prometheus_service_address}:{prometheus_service_port}"
     logger = Logger(PrometheusConnect(url=prometheus_url))
