0f6148b3e0d1637b73b3bdd6c69cdfac3c2946fc
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-25 22:32:11 +0200

    exp 1000

--- message-analyzer/src/queue/queue.ts
+++ message-analyzer/src/queue/queue.ts
@@ -47,7 +47,7 @@ export function addInQueue(
     task: TaskType
 ) {
     RabbitMQConnection.getChannel().then(async (channel: Channel) => {
-        const isPub = channel.publish(exchangeName, type, Buffer.from(JSON.stringify(task)), { expiration: '3000' });
+        const isPub = channel.publish(exchangeName, type, Buffer.from(JSON.stringify(task)), { expiration: '1000' });
         if (!isPub) console.log("REJECTED");
     })
 }
