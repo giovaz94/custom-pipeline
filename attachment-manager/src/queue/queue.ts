1c6ba1fe71b0422249b6c6625d7c0ef6c0ac6c10
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-26 00:03:00 +0200

    reduce queu lenght

--- attachment-manager/src/queue/queue.ts
+++ attachment-manager/src/queue/queue.ts
@@ -14,7 +14,7 @@ var consume: Replies.Consume;
 const prefetch = parseInt(process.env.PREFETCH as string, 10);
 
 async function enqueue(item: ConsumeMessage): Promise<void> {
-    if(queue.length < 50) {
+    if(queue.length < 5) {
         if (pendingPromises.length > 0) {
             const resolve = pendingPromises.shift();
             resolve!(item);
