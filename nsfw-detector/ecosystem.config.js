3256658c3d5ce059410ae2b54d54b29f946f08d2
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-26 14:01:18 +0200

    fork mode

--- nsfw-detector/ecosystem.config.js
+++ nsfw-detector/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: 'entrypoint',
         script: './dist/index.js',
         instances: 1,
-        exec_mode: 'cluster',
+        exec_mode: 'fork',
         watch: false,
         max_memory_restart: '200M',
         kill_timeout: 600000
