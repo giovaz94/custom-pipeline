9b35bf32a256f97a53723296ddc9dd504ab84c55
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-29 16:36:16 +0200

    refactor: change entrypoint

--- k8s/delete_all.sh
+++ k8s/delete_all.sh
@@ -1,9 +1,7 @@
 kubectl delete -f parser/ 
 kubectl delete -f attachment-manager/ 
 kubectl delete -f virus-scanner/ 
-kubectl delete -f image-analyzer/ 
-kubectl delete -f image-recognizer/ 
+kubectl delete -f image-analyzer/
 kubectl delete -f message-analyzer/ 
-kubectl delete -f gs-algorithm/ 
-kubectl delete -f nsfw-detector/
+kubectl delete -f gs-algorithm/
 kubectl get pods --no-headers | grep '^sys-pod' | awk '{print $1}' | xargs kubectl delete pod
