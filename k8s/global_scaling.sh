3b6e9c006a2d55c7a90c66220bcb22c4096bb2e8
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-07-31 13:23:02 +0200

    useful scripts

---
+++ k8s/global_scaling.sh
@@ -0,0 +1,12 @@
+kubectl apply -f parser/parser.yaml
+kubectl apply -f attachment-manager/attachment-manager.yaml
+kubectl apply -f image-analyzer/image-analyzer.yaml
+#kubectl apply -f image-recognizer/image-recognizer.yaml
+kubectl apply -f virus-scanner/virus-scanner.yaml
+kubectl apply -f message-analyzer/message-analyzer.yaml 
+kubectl apply -f gs-algorithm/gs-algorithm.yaml
+#kubectl apply -f nsfw-detector/nsfw-detector.yaml
+kubectl apply -f link-analyzer/link-analyzer.yaml 
+kubectl apply -f text-analyzer/text-analyzer.yaml 
+kubectl apply -f header-analyzer/header-analyzer.yaml 
+
---
+++ k8s/local_scaling.sh
@@ -0,0 +1,10 @@
+kubectl apply -f parser/
+kubectl apply -f attachment-manager/
+kubectl apply -f image-analyzer/
+kubectl apply -f virus-scanner/
+kubectl apply -f message-analyzer/
+#kubectl apply -f nsfw-detector/nsfw-detector.yaml
+kubectl apply -f link-analyzer/link-analyzer.yaml 
+kubectl apply -f text-analyzer/text-analyzer.yaml 
+kubectl apply -f header-analyzer/header-analyzer.yaml 
+
