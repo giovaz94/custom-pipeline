3e78604c9b320ca02aa023549e8b59b2326c6463
LBacchiani <lorenzo.bacchiani2@unibo.it>
2024-08-02 20:39:25 +0200

    fix

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -66,7 +66,7 @@ class Logger:
 if __name__ == "__main__":
 
     prometheus_service_address = "localhost"
-    prometheus_service_port = 56274
+    prometheus_service_port = 50700
     prometheus_url = f"http://{prometheus_service_address}:{prometheus_service_port}"
     logger = Logger(PrometheusConnect(url=prometheus_url))
 
