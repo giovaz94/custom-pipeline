ee8fba69eb7058ca052736152ce84bea87e579c3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-08-03 16:34:18 +0200

    test: complete proactiv-global runs

--- k8s/global_scaling.sh
+++ k8s/global_scaling.sh
@@ -4,7 +4,7 @@ kubectl apply -f image-analyzer/image-analyzer.yaml
 #kubectl apply -f image-recognizer/image-recognizer.yaml
 kubectl apply -f virus-scanner/virus-scanner.yaml
 kubectl apply -f message-analyzer/message-analyzer.yaml 
-#kubectl apply -f gs-algorithm/gs-algorithm.yaml
+kubectl apply -f gs-algorithm/gs-algorithm.yaml
 #kubectl apply -f nsfw-detector/nsfw-detector.yaml
 kubectl apply -f link-analyzer/link-analyzer.yaml 
 kubectl apply -f text-analyzer/text-analyzer.yaml 
